b777a0203f145ad51ce8b12d84bc00511a3e6849
shin88 <shin88@ejbt.co.kr>
2019-02-26 13:47:49 +0900

    logging 설정

--- src/main/java/com/enjoybt/common/config/DabaBaseConfig.java
+++ src/main/java/com/enjoybt/common/config/DabaBaseConfig.java
@@ -1,6 +1,5 @@
 package com.enjoybt.common.config;
 
-import com.zaxxer.hikari.HikariDataSource;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
@@ -9,10 +8,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 
 import javax.sql.DataSource;
-import java.io.IOException;
 
 @Configuration
 public class DabaBaseConfig {
